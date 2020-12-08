280c27f7d353737959e0dacb76fec6eb1b2b1a15
heinerbehrends <heinerbehrends@hotmail.com>
2020-12-08 11:04:38 +0100

    tweaked videowall size and pics

--- src/components/VideoWall.tsx
+++ src/components/VideoWall.tsx
@@ -2,16 +2,25 @@ import React from "react"
 import styled from "styled-components"
 import VideoWallNoise from "./VideoWallNoise"
 import VideoWallScreen from "./VideoWallScreen"
-import { Container } from "../styles/pageStyles"
 import VideoWallPic01 from "./VideoWallPic01"
 import VideoWallPic02 from "./VideoWallPic02"
 import VideoWallPic03 from "./VideoWallPic03"
 import VideoWallPic04 from "./VideoWallPic04"
 
-const VideoWallContainer = styled.div`
+interface videoContainerProps {
+  position: number
+}
+
+const VideoWallContainer = styled.div<videoContainerProps>`
   grid-area: 1 / 1;
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
+  max-width: 768px;
+  margin: 0 auto;
+  grid-area: 1 / 1;
+  z-index: -1;
+  will-change: transform;
+  transform: translate3d(0, ${props => props.position * -0.6}px, 0);
 `
 
 interface screensProps {
@@ -48,26 +57,20 @@ function NoiseScreens({ columns, rows }: screensProps): JSX.Element {
     </>
   )
 }
+
 interface videoWallProps {
   position: number
 }
 
 export default function VideoWall({ position }: videoWallProps): JSX.Element {
   return (
-    <VideoWallContainer
-      style={{
-        gridArea: "1 / 1",
-        zIndex: -1,
-        // transition: "transform 0.05s",
-        transform: `translateY(${-0.1 * position}px)`,
-      }}
-    >
-      <Screens columns={4} rows={24} />
+    <VideoWallContainer position={position}>
+      <Screens columns={4} rows={13} />
       <VideoWallPic01 />
-      <VideoWallPic02 />
+      {/* <VideoWallPic02 /> */}
       <VideoWallPic03 />
       <VideoWallPic04 />
-      <NoiseScreens columns={4} rows={24} />
+      <NoiseScreens columns={4} rows={13} />
     </VideoWallContainer>
   )
 }
