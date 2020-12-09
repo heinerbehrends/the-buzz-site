5a21d845792c1cd67cf06f3aad755ccaf2099a72
heinerbehrends <heinerbehrends@hotmail.com>
2020-12-09 16:38:19 +0100

    changed to gatsby image, tweaked styles

--- src/components/SocialContact.tsx
+++ src/components/SocialContact.tsx
@@ -3,21 +3,28 @@ import styled from "styled-components"
 
 const FlexContainer = styled.div`
   display: flex;
+  margin-top: 32px;
   place-content: center;
-  background-color: rgba(0, 0, 0, 0.66);
+  align-items: center;
+  justify-content: center;
   img {
     height: 100px;
   }
   a {
+    padding-left: 16px;
     color: rgba(256, 256, 256, 0.8);
     text-shadow: 0.05em 0.05em 0.03em #000;
+    text-decoration: none;
+    :hover {
+      text-decoration: underline;
+    }
   }
 `
 
-export default function SocialContact({ iconLink, link, linkText }) {
+export default function SocialContact({ icon, link, linkText }) {
   return (
     <FlexContainer>
-      <img src={iconLink} />
+      {icon}
       <a href={link}>{linkText}</a>
     </FlexContainer>
   )
