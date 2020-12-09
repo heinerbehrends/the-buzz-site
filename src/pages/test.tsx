81765c965be6fdaa38280876e6cf8686a3422b4e
heinerbehrends <heinerbehrends@hotmail.com>
2020-12-09 16:39:17 +0100

    removed unused imports

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -7,11 +7,10 @@ import Info from "../components/Info"
 import MultiplePlayers from "../components/MultiplePlayers"
 import ContactForm from "../components/ContactForm"
 import { GlobalStyle } from "../styles/globalStyles"
-import { Container, MainContainer } from "../styles/pageStyles"
+import { MainContainer } from "../styles/pageStyles"
 import { useScroll } from "../utils/useScroll"
 import { songs } from "../songs"
 import Footer from "../components/Footer"
-import EmailIcon from "../components/EmailIcon"
 
 const FixedContainer = styled.div`
   position: fixed;
