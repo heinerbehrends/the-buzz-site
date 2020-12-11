1620e7e4943e87e324b175e91c7277d5f1412240
heinerbehrends <heinerbehrends@hotmail.com>
2020-12-11 23:13:59 +0100

    moved /test to index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import styled from "styled-components"
 import VideoWall from "../components/VideoWall"
 import Header from "../components/Header"
 import { MainHeading, ConnectHeading } from "../styles/headingStyles"
@@ -6,24 +7,38 @@ import Info from "../components/Info"
 import MultiplePlayers from "../components/MultiplePlayers"
 import ContactForm from "../components/ContactForm"
 import { GlobalStyle } from "../styles/globalStyles"
-import { Container, MainContainer } from "../styles/pageStyles"
-import { useScroll } from "../utils/useScroll"
+import { MainContainer } from "../styles/pageStyles"
+import Footer from "../components/Footer"
 import { songs } from "../songs"
+import useParallax from "../utils/useParallax"
+
+const FixedContainer = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+`
 
 export default function Home() {
-  const scrollTop = useScroll()
+  useParallax()
   return (
-    <Container>
+    <>
       <GlobalStyle />
-      <VideoWall position={scrollTop} />
-      <MainContainer>
-        <Header />
-        <MainHeading>what's the Buzz all about?</MainHeading>
-        <Info />
-        <MultiplePlayers songs={songs} />
-        <ConnectHeading>connect with the Buzz</ConnectHeading>
-        <ContactForm />
-      </MainContainer>
-    </Container>
+      <FixedContainer>
+        <VideoWall id={"background"} />
+      </FixedContainer>
+      <FixedContainer>
+        <MainContainer id={"foreground"}>
+          <Header />
+          <MainHeading>what's the Buzz all about?</MainHeading>
+          <Info />
+          <MultiplePlayers songs={songs} />
+          <ConnectHeading>connect with the Buzz</ConnectHeading>
+          <ContactForm />
+          <Footer />
+        </MainContainer>
+      </FixedContainer>
+    </>
   )
 }
