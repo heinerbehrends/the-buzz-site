8fcf78405ae00ea7042d3a1d11fc9fdfaf7da3a3
heinerbehrends <heinerbehrends@hotmail.com>
2020-12-11 23:17:48 +0100

    Added responsive font size

--- src/styles/playerStyles.ts
+++ src/styles/playerStyles.ts
@@ -5,7 +5,7 @@ export const Credits = styled.p`
   color: rgba(256, 256, 256, 0.8);
   text-align: right;
   text-align: right;
-  font-size: 2.5vw;
+  font-size: max(2.083vw, 10px);
   line-height: 130%;
   padding: 0;
   padding-right: 1.2em;
@@ -22,17 +22,25 @@ export const hoverStyle = css`
   }
 `
 
-export const DownloadLink = styled.a`
-  color: rgba(256, 256, 256, 0.8);
-  padding-left: 3.5vw;
-  text-decoration: none;
-   ${hoverStyle}
+const mediumFontSize = css`
+  font-size: max(2.34vw, 14px);
   @media screen and (min-width: 768px) {
     font-size: 18px;
   }
 `
 
+export const DownloadLink = styled.a`
+  color: rgba(256, 256, 256, 0.8);
+  padding-left: 3.5vw;
+  text-decoration: none;
+  ${hoverStyle}
+  ${mediumFontSize}
+`
+
 export const TimeDisplayStyled = styled(TimeDisplay)`
-  margin-right: 16px;
-  font-weight: 500;
+  margin-right: 2.083vw;
+  @media screen and (min-width: 768px) {
+    margin-right: 16px;
+  }
+  ${mediumFontSize}
  `;
