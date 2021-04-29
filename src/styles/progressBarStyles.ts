52c31f999c51347be9e573a2cf4313518f8eb684
heinerbehrends <heinerbehrends@hotmail.com>
2021-04-29 13:47:23 +0200

    removed buffer bar and time handle

--- src/styles/progressBarStyles.ts
+++ src/styles/progressBarStyles.ts
@@ -1,5 +1,4 @@
-import styled, {css} from "styled-components"
-
+import styled, { css } from "styled-components"
 
 export const BarStyles = css`
   grid-area: 1 / 1;
@@ -12,16 +11,6 @@ export const BackgroundBar = styled.div`
   background-color: gray;
 `
 
-interface bufferedBarProps {
-  buffered: number
-}
-export const BufferedBar = styled.div<bufferedBarProps>`
-${BarStyles}
-  z-index: 1;
-  width: ${props => props.buffered}%;
-  background-color: darkergray;
-`
-
 export const ElapsedBar = styled.div`
   ${BarStyles}
   z-index: 2;
@@ -29,45 +18,23 @@ export const ElapsedBar = styled.div`
   box-shadow: var(--white-glow);
 `
 export const ClickableBar = styled.div`
-${BarStyles}
+  ${BarStyles}
   background-color: transparent;
   cursor: pointer;
   z-index: 3;
   height: 40px;
   grid-area: 1 /1;
   :hover ~ span {
-    opacity: 1
-  }
-`
-
-interface timeHandleProps {
-  elapsed: number
-  offset: number
-}
-export const TimeHandle = styled.span<timeHandleProps>`
-  position: relative;
-  opacity: 0;
-  transition: opacity 0.25s;
-  grid-area: 1 /1;
-  z-index: 4;
-  width: 16px;
-  height: 16px;
-  border-radius: 8px;
-  background-color: var(--white-color);
-  transform: translateX(${props => props.offset - 8}px);
-  /* left: ${({elapsed, offset}) => elapsed + offset}%; */
-  cursor: grab;
-  :hover {
     opacity: 1;
   }
 `
 
 export const ProgressBarContainer = styled.div`
   display: grid;
+  flex: auto;
   width: 100%;
   align-items: center;
   border-radius: 4px;
-  margin-right: 20px;
 `
 export const Invisible = styled.div`
   opacity: 0;
