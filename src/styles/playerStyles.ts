63cc7603a0710a819b75bc7aab702800b229bf98
heinerbehrends <heinerbehrends@hotmail.com>
2021-04-29 13:46:53 +0200

    changed padding

--- src/styles/playerStyles.ts
+++ src/styles/playerStyles.ts
@@ -1,5 +1,5 @@
-import styled, {css} from "styled-components";
-import TimeDisplay from "../components/TimeDisplay";
+import styled, { css } from "styled-components"
+import TimeDisplay from "../components/TimeDisplay"
 
 export const Credits = styled.p`
   color: rgba(256, 256, 256, 0.8);
@@ -8,12 +8,11 @@ export const Credits = styled.p`
   font-size: max(2.083vw, 10px);
   line-height: 130%;
   padding: 0;
-  padding-right: 1.2em;
+  padding-right: 2rem;
   margin: 0;
 
   @media screen and (min-width: 768px) {
     font-size: 16px;
-    padding-right: 28px;
   }
 `
 export const hoverStyle = css`
@@ -30,17 +29,17 @@ const mediumFontSize = css`
 `
 
 export const DownloadLink = styled.a`
+  padding-left: 0.5rem;
   color: rgba(256, 256, 256, 0.8);
-  padding-left: 3.5vw;
   text-decoration: none;
   ${hoverStyle}
   ${mediumFontSize}
 `
 
 export const TimeDisplayStyled = styled(TimeDisplay)`
-  margin-right: 2.083vw;
-  @media screen and (min-width: 768px) {
-    margin-right: 16px;
-  }
   ${mediumFontSize}
- `;
+  display: none;
+  @media screen and (min-width: 480px) {
+    display: inline;
+  }
+`
