6c688e33bece6c3d21ff0d0ad425cc5d0b941874
heinerbehrends <heinerbehrends@hotmail.com>
2021-07-07 13:21:18 +0200

    better names for magic numbers, removed unused import

--- src/components/VideoWall.tsx
+++ src/components/VideoWall.tsx
@@ -3,7 +3,6 @@ import styled from "styled-components"
 import VideoWallNoise from "./VideoWallNoise"
 import VideoWallScreen from "./VideoWallScreen"
 import VideoWallPic01 from "./VideoWallPic01"
-import VideoWallPic02 from "./VideoWallPic02"
 import VideoWallPic03 from "./VideoWallPic03"
 import VideoWallPic04 from "./VideoWallPic04"
 
@@ -66,22 +65,22 @@ interface videoWallProps {
 }
 
 function VideoWall({ id, bodyHeight, bodyWidth }: videoWallProps): JSX.Element {
-  function calculateRows(width, height) {
-    const ratio = 1.2522
+  function calculateRows(width: number, height: number) {
+    const SCREEN_RATIO = 1.2522
+    const SCREEN_WIDTH = 153.33
     if (!width) {
       return 1
     }
     if (width >= 768) {
-      return Math.floor(height / 153.33)
+      return Math.floor(height / SCREEN_WIDTH)
     }
-    return Math.floor(width / 4 / ratio)
+    return Math.floor(width / 4 / SCREEN_RATIO)
   }
   const rows = calculateRows(bodyWidth, bodyHeight)
   return (
     <VideoWallContainer id={id}>
       <Screens columns={4} rows={rows} />
       <VideoWallPic01 />
-      {/* <VideoWallPic02 /> */}
       <VideoWallPic03 />
       <VideoWallPic04 />
       <NoiseScreens columns={4} rows={rows} />
