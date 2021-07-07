edc548ed4da2876607204fec00d7dd5b237713c5
heinerbehrends <heinerbehrends@hotmail.com>
2021-07-07 16:34:36 +0200

    changed to placeholder: none

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import { StaticImage } from "gatsby-plugin-image"
 
 function Header(): JSX.Element {
-  return <StaticImage src="../images/header.png" alt="" />
+  return <StaticImage src="../images/header.png" alt="" placeholder="none" />
 }
 
 export default React.memo(Header)
