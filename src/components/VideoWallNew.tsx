c08e622632cd33a42baf3e378c4cc311ac0c941c
heinerbehrends <heinerbehrends@hotmail.com>
2021-07-07 17:22:42 +0200

    removed blurred

--- src/components/VideoWallNew.tsx
+++ src/components/VideoWallNew.tsx
@@ -7,7 +7,7 @@ export default function VideoWallNew() {
       src="../images/videowall-new.jpg"
       alt="background image"
       width={768}
-      placeholder={"blurred"}
+      placeholder="none"
     />
   )
 }
